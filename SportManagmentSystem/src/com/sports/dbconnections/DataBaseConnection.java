0a19d771b18b342934552030703e9a9773fd585f
chavansam30 <chavansam30@gmail.com>
2017-01-28 16:12:38 +0530

    new web project

---
+++ SportManagmentSystem/src/com/sports/dbconnections/DataBaseConnection.java
@@ -0,0 +1,24 @@
+package com.sports.dbconnections;
+
+import java.sql.*;
+
+public class DataBaseConnection {
+
+	private static Connection con;
+	
+	public static Connection getCon() {
+		return con;
+	}
+
+	public static void setCon(Connection con) {
+		DataBaseConnection.con = con;
+	}
+
+	public DataBaseConnection() throws SQLException, ClassNotFoundException{
+		Class.forName("com.mysql.jdbc.Driver");  	
+		con = DriverManager.getConnection
+				("jdbc:mysql://localhost:3306/dervansports", "root", "sampada30");
+		setCon(con);
+	}
+	
+}
